dfbe16e950a73c67be92a8971cdbc3cc6ff6509c
imaculate-echesa <immaculatezepwa18@gmail.com>
2022-09-04 12:00:26 +0300

    complete project

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const input = document.getElementById("search-box")
 fetch("https://anapioficeandfire.com/api/books")
 .then((res)=>res.json())
 .then((data) => {
-  // console.log(data[0].name) 
   let bookList ="" 
   
   data.map(values => {
@@ -20,8 +19,6 @@ fetch("https://anapioficeandfire.com/api/books")
     
     let searchResult = ""
     const bookDetails = document.getElementById("book-type")
-    // console.log(inputValue)
-    // console.log(nameValue)
     function searched (){
       if (inputValue == nameValue){
         searchResult = `<p>
@@ -35,44 +32,14 @@ fetch("https://anapioficeandfire.com/api/books")
         MEDIATYPE: ${values.mediaType} </br>
         RELEASED: ${values.released}                                
         <p>`
-        // const authorName = values.name
-        // console.log(values.isbn)
-        // console.log(values.url)
-        // console.log(`${values.authors[0]}, ${values.authors[1]}` )
-        // console.log(values.publisher)
-        // console.log(values.country)
-        // console.log(values.mediaType)
-        // console.log(values.released)
+        
         bookDetails.innerHTML=searchResult
       }
 
     }
     
     searched()
-    
-    // console.log(values.isbn)
-    // console.log(searched)
-    
-    
-    // let bookSearch =""
-    // book = values.filter(input.value == values.name)
-    // console.log(book)
-    
-
-    // console.log(values.name)
-    
-    
-    // for (let i in values) {
-              // text += person[x];
-      // (input.value == i.name) ? 
-        // bookSearch = "<p>${values.name}<p>" : 
-                // console.log("No Book Found")
-        // console.log(i[0])
-      // }
-
-
-      // console.log(bookSearch)
   });
-    // innerHTML=bookSearch
+  
 })
 })
